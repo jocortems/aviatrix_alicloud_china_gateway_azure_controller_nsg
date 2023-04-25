0fe31181528eea45317418233a78a36bf4217f48
Ubuntu <jcortes@linux-vm.oziqepeb4yde5ai0zxn04kua1e.jx.internal.cloudapp.net>
2023-04-25 19:22:47 +0000

    Added more outputs

--- outputs.tf
+++ outputs.tf
@@ -3,5 +3,13 @@ output "gateway_address" {
 }
 
 output "gatewayha_address" {
-  value = var.ha_enabled ? [for eip in data.alicloud_eip_addresses.avx_gwha[0].addresses : eip.ip_address] : null  
+  value = local.gatewayha_address
+}
+
+output "gateway_eip_id" {
+  value = [for eip in data.alicloud_eip_addresses.avx_gw.addresses : eip.allocation_id]
+}
+
+output "gatewayha_eip_id" {
+  value = var.ha_enabled ? [for eip in data.alicloud_eip_addresses.avx_gwha[0].addresses : eip.allocation_id] : null
 }
