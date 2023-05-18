fae296efe9026745eeb2fd280956dd06350b3d41
Ubuntu <jcortes@linux-vm.oziqepeb4yde5ai0zxn04kua1e.jx.internal.cloudapp.net>
2023-05-18 18:45:32 +0000

    Updated Versions

--- versions.tf
+++ versions.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.52.0"
+      version = ">= 3.52.0"
       configuration_aliases = [azurerm.controller]
     }
     alicloud = {
       source = "aliyun/alicloud"
-      version = "~> 1.204.0"
+      version = ">= 1.200.0"
       configuration_aliases = [alicloud.china]
     }
 }
